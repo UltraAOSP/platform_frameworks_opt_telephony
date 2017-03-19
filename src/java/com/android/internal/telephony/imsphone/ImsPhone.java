ec7678d8f1f71c6bf145d7b8711b406f0b9dfb30
Ahmed Hady <ahmedhady6@gmail.com>
2017-03-19 03:11:48 +0200

    Revert Some wrong commits

--- src/java/com/android/internal/telephony/imsphone/ImsPhone.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhone.java
@@ -934,7 +934,7 @@ public class ImsPhone extends ImsPhoneBase {
 
         try {
             ImsUtInterface ut = mCT.getUtInterface();
-            ut.queryCallBarring(getCBTypeFromFacility(facility), serviceClass, resp);
+            ut.queryCallBarring(getCBTypeFromFacility(facility), resp);
         } catch (ImsException e) {
             sendErrorResponse(onComplete, e);
         }
